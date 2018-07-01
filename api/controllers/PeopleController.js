a60e0bb27835dcdbea7fde4056800a70dc0783be
javiercordero <javier.cordero@upr.edu>
2018-07-01 06:55:59 -0400

    Renombré archivos y borré otros innecesarios.

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -1,19 +1,19 @@
 /**
- * GenerateController
+ * PeopleController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
-  
+
 
   /**
    * `GenerateController.PersonReport()`
    */
-  PersonReport: async function (req, res) {
+  PeopleReport: async function (req, res) {
     return res.json({
-      todo: 'PersonReport() is not implemented yet!'
+      todo: 'PeopleReport() is not implemented yet!'
     });
   }
 
