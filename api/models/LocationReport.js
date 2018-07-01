d0d67843c1722f88a6cbdd39d2ca572a0c8e709f
va2ron1 <va2ron1@hotmail.com>
2018-06-30 23:15:46 -0400

    add the atrritubte neccesary to each model created

--- api/models/LocationReport.js
+++ api/models/LocationReport.js
@@ -12,29 +12,36 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-   event:{
-
-   },
     type:{
-
+      type: 'string',
+      isIn: ['break', 'through'],
+      required: true
    },
     place:{
-
+      type: 'string',
+      required: true
     },
     lat:{
-
+      type: 'number',
+      required: true
     },
-    lon:{
-
+    lng:{
+      type: 'number',
+      required: true
     },
     description:{
-
+      type: 'string',
+      required: false
     },
     timestamp:{
-
+      type: 'string',
+      columnType: 'timestamp',
+      required: true
     },
     state:{
-
+      type: 'string',
+      isIn: ['collapsed', 'draft'],
+      defaultsTo: 'draft'
     },
 
 
--- api/models/PersonReport.js
+++ api/models/PersonReport.js
@@ -12,8 +12,48 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    fUllName: {
+      type: 'string',
+      required: true,
+      description: 'Full representation of the user\'s name',
+      maxLength: 120,
+      example: 'Lisa'
+    },
+    lastApperance:{
+      type: 'timestamp'
+    },
+    type:{
+      type: 'string',
+      isIn: ['break', 'through'],
+      required: true
+    },
+    place:{
+      type: 'string',
+      required: true
+    },
+    lat:{
+      type: 'number',
+      required: true
+    },
+    lng:{
+      type: 'number',
+      required: true
+    },
+    description:{
+      type: 'string',
+      required: false
+    },
+    timestamp:{
+      type: 'string',
+      columnType: 'timestamp',
+      required: true
+    },
+    state:{
+      type: 'string',
+      isIn: ['disappeared', 'dead', 'draft'],
+      defaultsTo: 'draft'
+    },
 
-    PersonReport: { type: 'string' }
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
